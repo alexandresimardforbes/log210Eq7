6fdca8a0133eb0d03e6d9ad6e79cf30bd4343bab
alexandresimardforbes <alexandresimardforbes@gmail.com>
2017-09-29 15:08:17 -0400

    [CU1] Gérer un compte usager - Remove some impertinent data in the json such as the encrypt password. - Add the migration for the UUID. (I forget to add it in my last push)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
-
   def create
     user = User.new(user_params)
     if user.save
-      #render json: {status: 'Un nouvel usager à été créé'}, status: :created
-      render json: user
+      render json: user, except: [:password_digest, :created_at,
+                                  :uuid, :updated_at]
     else
-      render json: {errors: user.errors.full_messages }, status: :bad_request
+      render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
 end
