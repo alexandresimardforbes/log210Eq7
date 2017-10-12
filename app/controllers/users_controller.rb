90d7a536edac639838df3070aa45270cc761eecf
alexandresimardforbes <alexandresimardforbes@gmail.com>
2017-10-12 13:58:33 -0400

    [CU1] Gérer un compte usager - Removes attribut uuid from user and add disable. - Added show (show a user) and index (show all users) method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,41 @@
 class UsersController < ApplicationController
+  before_action :fetch_user, only: %i[show update destroy]
+
+  # http: GET /users
+  def index
+    users = User.all
+    json_response(users)
+  end
+
+  # http: GET /users/:id
+  def show
+    render json: @user, except: %i[password_digest created_at
+                                    updated_at]
+  end
+
+  # http: POST /users
   def create
     user = User.new(user_params)
     if user.save
       render json: user, except: %i[password_digest created_at
-                                    uuid updated_at]
+                                    updated_at]
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
 
+  # http: PATCH /users/:id
   def update
-    fetch_user
     if @user.update_attributes(user_params)
       render json: @user, except: %i[password_digest created_at
-                                     uuid updated_at]
+                                     updated_at]
     else
       render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
   end
 
+  # http: DELETE /users/:id
   def destroy
-    fetch_user
     if @user.destroy
       head 200
     else
@@ -28,14 +43,20 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
+
   def fetch_user
     @user = User.find_by_id(params[:id])
   end
 
-  private
-
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,
-                                 :first_name, :last_name, :user_type)
+                                 :first_name, :last_name, :user_type, :disable)
+  end
+
+  def json_response(object, status = :ok)
+    render json: object, status: status, except: %i[password_digest created_at
+                                   updated_at]
   end
 end
---
+++ db/migrate/20171012170919_add_column_to_user_disable.rb
@@ -0,0 +1,5 @@
+class AddColumnToUserDisable < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :disable, :bool
+  end
+end
