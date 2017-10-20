1fd8ddaf1ed591bcfb25a6d12fd05ce62089e6fb
tristansanterre <tristan.santerre.1@ens.etsmtl.ca>
2017-10-20 17:34:49 -0400

    Commit referents

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :referents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :update, :destroy, :index, :show]
   resources :organisme_referents, only: [:create, :update, :destroy, :index, :show]
