7fc6e06ba63cd937970808d01e999f1869ccfd9b
piotte13 <jeremie.piotte@gmail.com>
2017-10-20 14:58:38 -0400

    connection to backend

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
 import { AuthService } from './auth.service';
 import { UsersPageComponent } from './users-page/users-page.component';
-import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [UsersService]
+  providers: []
 })
 export class AppComponent {
   title = 'app';
