dc26602c43ef39d5d90262aa8d8aba6a9f065647
alexandresimardforbes <alexandresimardforbes@gmail.com>
2017-11-17 18:27:01 -0500

    [Seed] - Add referers seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,6 +33,17 @@
                                        phone_b:'7672625',fax:'56789089', email:'marcl@ghot.com',preference_fax: true, preference_courriel: false, preference_papier: false,
                                        organisme_referent_id:'1')
 
+    @referentDeuxième = Referent.create(first_name: 'Alain',last_name: 'Coté',title:'Psycologue',phone_c:'111111',
+                                       phone_b:'2222222',fax:'9999999', email:'alianCote@ghot.com',preference_fax: true, preference_courriel: false, preference_papier: false,
+                                       organisme_referent_id:'2')
+
+    @referentQuatrieme = Referent.create(first_name: 'Gilbert',last_name: 'Caron',title:'Coclown',phone_c:'56746r784',
+                                       phone_b:'7873487',fax:'46784', email:'GilbC@ghot.com',preference_fax: true, preference_courriel: false, preference_papier: false,
+                                       organisme_referent_id:'1')
+
+    @referentTroisieme = Referent.create(first_name: 'Sacha',last_name: 'Rozon',title:'Mascotte',phone_c:'6666666',
+                                        phone_b:'7777777',fax:'8888888', email:'sachacote@ghot.com',preference_fax: true, preference_courriel: false, preference_papier: false,
+                                        organisme_referent_id:'1')
 
     @organismeAlpha = Organisme.create(nom: 'Les amis d''entraide',telephone: '1111111111',
                                             fax: '2222222',courriel: 'amientre@gmaon.com',
